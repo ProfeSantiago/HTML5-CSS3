b47c782d8c57659c2f5897c49f3d463351494273
Unibitec <Unibitec@users.noreply.github.com>
2019-03-04 06:47:29 -0600

    Primera Subida
    Primera Subida

---
+++ 6) Sitio Completo de Ejemplo/scripts/MasterJS.js
@@ -0,0 +1,13 @@
+﻿/***********************************************
+Lic. Santiago Rodríguez P. Updated: 01-01-15
+***********************************************/
+var x=$(document);
+	x.ready(inicializarEventos); //Se ejecuta al cargar la pagina.
+	
+	function inicializarEventos() {	
+		$( "input[type=submit], a, button" ).button();	
+
+		PonePunteroManita('.miLink'); //Le pone el puntero de manita al elemento
+	}// Fin inicializarEventos	
+//------------------------------------------------------------------------
+
